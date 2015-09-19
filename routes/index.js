802de8df1554c91fcef0eed3fc4a4422550d6535
Craig Coles <craigcoles8@gmail.com>
2015-09-19 21:19:44 +0100

    All the data

--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,47 @@ var express     = require('express'),
     dateFormat  = require('dateformat');
     router      = express.Router(),
     matchData   = './data/matches.json',
-    personData  = './data/people.json';
+    personData  = './data/people.json',
+    mData = '',
+    pData = '';
 
-fs.readFile(matchData, 'utf8', function (err, data) {
+    fs.readFile(matchData, 'utf8', function (err, data) {
 
-    if (err) {
-        console.log('Error: ' + err);
-        return;
-    }
+        if (err) {
+            console.log('Error: ' + err);
+            return;
+        }
 
-    data = JSON.parse(data);
+        mData = JSON.parse(data);
+
+    });
+
+    fs.readFile(personData, 'utf8', function (err, data) {
+
+        if (err) {
+            console.log('Error: ' + err);
+            return;
+        }
+
+        pData = JSON.parse(data);
+
+    });
 
     // for (var i = 0; i < data.matches.length; i++) {
-    for (var i = 0; i < 1; i++) {
+    // for (var i = 0; i < 1; i++) {
 
-        var matchDate = new Date(data.matches[i].time.millis);
+    //     var matchDate = new Date(data.matches[i].time.millis);
 
-        console.dir( dateFormat(matchDate, "dddd dS mmmm") );
-        console.dir( dateFormat(matchDate, "h:MM TT") );
-        console.dir( data.matches[i].venue.name + ', ' + data.matches[i].venue.city + ', ' + data.matches[i].venue.country );
-        console.dir( data.matches[i].teams[0].name + ' vs ' + data.matches[i].teams[1].name );
+    //     console.dir( dateFormat(matchDate, "dddd dS mmmm") );
+    //     console.dir( dateFormat(matchDate, "h:MM TT") );
+    //     console.dir( data.matches[i].venue.name + ', ' + data.matches[i].venue.city + ', ' + data.matches[i].venue.country );
+    //     console.dir( data.matches[i].teams[0].name + ' vs ' + data.matches[i].teams[1].name );
 
-    }
+    // }
 
-});
+    /* GET Home page. */
+    router.get('/', function(req, res) {
+        res.render('pages/home', [mData, pData]);
+    });
 
-/* GET Home page. */
-router.get('/', function(req, res) {
-    console.dir(matchJson);
-    res.render('pages/home');
-});
-
-module.exports = router;
+    module.exports = router;
