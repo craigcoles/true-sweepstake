cda7ba428b8bfa886764245b2f1166458cb8fae2
Craig Coles <craig.coles@truedigital.co.uk>
2015-09-21 15:28:56 +0100

    Moving forward

--- routes/index.js
+++ routes/index.js
@@ -29,21 +29,15 @@ var express     = require('express'),
 
     });
 
-    // for (var i = 0; i < data.matches.length; i++) {
-    // for (var i = 0; i < 1; i++) {
-
-    //     var matchDate = new Date(data.matches[i].time.millis);
-
-    //     console.dir( dateFormat(matchDate, "dddd dS mmmm") );
-    //     console.dir( dateFormat(matchDate, "h:MM TT") );
-    //     console.dir( data.matches[i].venue.name + ', ' + data.matches[i].venue.city + ', ' + data.matches[i].venue.country );
-    //     console.dir( data.matches[i].teams[0].name + ' vs ' + data.matches[i].teams[1].name );
-
-    // }
-
     /* GET Home page. */
     router.get('/', function(req, res) {
-        res.render('pages/home', [mData, pData]);
+
+        res.render('pages/home', {
+            mData: mData.matches,
+            pData: pData,
+            dateFormat: dateFormat
+        });
+
     });
 
     module.exports = router;
